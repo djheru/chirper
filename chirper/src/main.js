6aa8c52aab1504f523851fed996ca916683550b7
Philip Damra <djheru@gmail.com>
2015-10-26 22:42:08 -0400

    App component and router

--- chirper/src/main.js
+++ chirper/src/main.js
@@ -1,3 +1,14 @@
-var API = require('./api'),
+var React = require('react'),
+  ReactRouter = require('react-router'),
+  Route = ReactRouter.Route,
+  App = require('./components/App'),
+  API = require('./api'),
   chirps = require('./stores/chirps');
+
+var routes = (
+  <Route handler={App}></Route>
+);
 API.fetchChirps();
+ReactRouter.run(routes, ReactRouter.HistoryLocation, function (Root) {
+  React.render(<Root />, document.getElementById('app'));
+});

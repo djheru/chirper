002ade2e90b8ca3d3644a91ad0a7a18e0e7ee685
Philip Damra <djheru@gmail.com>
2015-10-29 00:26:09 -0400

    navigation component

--- chirper/src/components/App.js
+++ chirper/src/components/App.js
@@ -1,5 +1,6 @@
 var React = require('react'),
-  RouteHandler = require('react-router').RouteHandler;
+  RouteHandler = require('react-router').RouteHandler,
+  Navigation = require('./Navigation');
 
 var App = React.createClass({
   render: function () {
@@ -11,7 +12,7 @@ var App = React.createClass({
 
         <div className="row">
           <div className="three columns">
-            Navigation
+            <Navigation />
           </div>
           <div className="nine columns">
             <RouteHandler />
---
+++ chirper/src/components/Navigation.js
@@ -0,0 +1,24 @@
+var React = require('react'),
+  Link = require('react-router').Link,
+  UserStore = require('../stores/users');
+
+var Navigation = React.createClass({
+
+  getInitialState: function () {
+    return {
+      username: UserStore.currentUser.username
+    };
+  },
+
+  render: function () {
+    return (
+      <ul>
+        <li><Link to='home'>Timeline</Link></li>
+        <li><Link to='users'>Users</Link></li>
+        <li><a href='/logout'>Logout </a> ({this.state.username})</li>
+      </ul>
+    );
+  }
+});
+
+module.exports = Navigation;
